f24d87dc3bb047121e3e9b5c03b91beae693f055
Arnab Das <imarnab01@gmail.com>
2020-07-20 14:46:42 +0530

    change

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,18 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
   constructor() {}
+
+  // flag: boolean = true;
+
   Job_data: Object[] = [
     {
-      tag1: 'Full Time',
-      tag2: 'Hourly',
-      tag3: 'Part Time',
-      job_title: 'Software Engeneer',
-      posted: '5 Days Ago',
-      count: '25',
-      company: 'American Express',
-      package: '20 LPA',
-      job_type: 'Part Time',
-      location: 'Hyderabad',
+      event_type: 'upcoming',
+      event_data: {
+        tag1: 'Full Time',
+        tag2: 'Hourly',
+        tag3: 'Part Time',
+        status: 'In Progress ...',
+        job_title: 'Software Engeneer',
+        posted: '5 Days Ago',
+        count: '25',
+        company: 'American Express',
+        package: '20 LPA',
+        job_type: 'Part Time',
+        location: 'Hyderabad',
+      },
+    },
+    {
+      event_type: 'live',
+      event_data: {
+        tag1: 'Full Time',
+        tag2: 'Hourly',
+        tag3: 'Part Time',
+        status: 'In Progress ...',
+        job_title: 'Software Engeneer',
+        posted: '5 Days Ago',
+        count: '25',
+        company: 'American Express',
+        package: '20 LPA',
+        job_type: 'Part Time',
+        location: 'Hyderabad',
+      },
     },
   ];
   ngOnInit(): void {}
