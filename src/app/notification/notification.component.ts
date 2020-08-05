73d2e1b9802fdfa150c2dd83368bc6a79ae99cae
Arnab  Das <imarnab01@gmail.com>
2020-08-05 14:36:52 +0530

    notification complete

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -12,69 +12,80 @@ export class NotificationComponent implements OnInit {
 
   Data: Object[] = [
     {
-      posted: '7:32',
-      header: 'Notifiaction: blah blah blah',
-      content: 'lorem ipsum lorem asdfgh',
-      button: 'Agree',
-      src:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
+      day: 'Today',
+      days_data: [
+        {
+          posted: '7:32',
+          header: 'Notifiaction: blah blah blah',
+          content: 'lorem ipsum lorem asdfgh',
+          button: 'Agree',
+          src:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
+        },
+        {
+          posted: '7:32',
+          header: 'Notifiaction: blah blah blah',
+          content: 'lorem ipsum lorem asdfgh',
+          button: 'Agree',
+          src:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
+        },
+        {
+          posted: '7:32',
+          header: 'Notifiaction: blah blah blah',
+          content: 'lorem ipsum lorem asdfgh',
+          button: 'Agree',
+          src:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
+        },
+        {
+          posted: '7:32',
+          header: 'Notifiaction: blah blah blah',
+          content: 'lorem ipsum lorem asdfgh',
+          button: 'Agree',
+          src:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
+        },
+      ],
     },
+
     {
-      posted: '7:32',
-      header: 'Notifiaction: blah blah blah',
-      content: 'lorem ipsum lorem asdfgh',
-      button: 'Agree',
-      src:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
+      day: 'Tommorow',
+      days_data: [
+        {
+          posted: '7:32',
+          header: 'Notifiaction: blah blah blah',
+          content: 'lorem ipsum lorem asdfgh',
+          button: 'Agree',
+          src:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
+        },
+        {
+          posted: '7:32',
+          header: 'Notifiaction: blah blah blah',
+          content: 'lorem ipsum lorem asdfgh',
+          button: 'Agree',
+          src:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
+        },
+        {
+          posted: '7:32',
+          header: 'Notifiaction: blah blah blah',
+          content: 'lorem ipsum lorem asdfgh',
+          button: 'Agree',
+          src:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
+        },
+        {
+          posted: '7:32',
+          header: 'Notifiaction: blah blah blah',
+          content: 'lorem ipsum lorem asdfgh',
+          button: 'Agree',
+          src:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
+        },
+      ],
     },
-    {
-      posted: '7:32',
-      header: 'Notifiaction: blah blah blah',
-      content: 'lorem ipsum lorem asdfgh',
-      button: 'Agree',
-      src:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
-    },
-    {
-      posted: '7:32',
-      header: 'Notifiaction: blah blah blah',
-      content: 'lorem ipsum lorem asdfgh',
-      button: 'Agree',
-      src:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
-    },
-    // {
-    //   posted: '7:32',
-    //   header: 'Notifiaction: blah blah blah',
-    //   content: 'lorem ipsum lorem asdfgh',
-    //   button: 'Agree',
-    //   src:
-    //     'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
-    // },
-    // {
-    //   posted: '7:32',
-    //   header: 'Notifiaction: blah blah blah',
-    //   content: 'lorem ipsum lorem asdfgh',
-    //   button: 'Agree',
-    //   src:
-    //     'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
-    // },
-    // {
-    //   posted: '7:32',
-    //   header: 'Notifiaction: blah blah blah',
-    //   content: 'lorem ipsum lorem asdfgh',
-    //   button: 'Agree',
-    //   src:
-    //     'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
-    // },
-    // {
-    //   posted: '7:32',
-    //   header: 'Notifiaction: blah blah blah',
-    //   content: 'lorem ipsum lorem asdfgh',
-    //   button: 'Agree',
-    //   src:
-    //     'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZuLiRaxAxDyHlETehHLegQD4plQoxBMtBgA&usqp=CAU',
-    // },
   ];
 
   onTabChange(event) {
